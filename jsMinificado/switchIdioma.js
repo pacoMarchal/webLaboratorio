4e417d470c9dd4e49f7c3e4ed8868d3f261fcbfd
pacoMarchal <paco.extremera@gmail.com>
2025-09-28 14:55:16 +0200

    Delete jsMinificado directory

--- jsMinificado/switchIdioma.js
+++
@@ -1,7 +0,0 @@
-const toggle=document.getElementById("languageToggle");
-// const text = document.getElementById("textContent");
-toggle.addEventListener("change",function(){this.checked?(alert("Cambiado a inglés"),
-// text.textContent = "Hello! Welcome to my website."
-):(alert("Cambiado a español"),
-// text.textContent = "¡Hola! Bienvenido a mi sitio web."
-)});
